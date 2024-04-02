3d276df54b7679cf1dc127a89b72f68aa2cb1f96
mashuq0068 <mashuq0068@gmail.com>
2024-04-02 17:33:13 +0600

    added category news

--- index.js
+++ index.js
@@ -23,39 +23,66 @@ async function run() {
     await client.connect();
     const database = client.db("twistBuzzDB")
     const newsCollection = database.collection("news")
-    app.post('/news' , async(req, res) => {
+    app.post('/news', async (req, res) => {
       const news = req.body
       const result = await newsCollection.insertOne(news)
       res.send(result)
     }
     )
-    app.get('/news/:id', async(req, res) => {
+
+    app.get('/news/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const news = await newsCollection.findOne(query)
       res.send(news)
     })
-    app.get('/allNews', async(req, res) => {
+
+    app.get('/allNews', async (req, res) => {
       const skipPages = parseInt(req.query.skipPages)
       const perPageData = parseInt(req.query.pageData)
-      console.log(skipPages , perPageData)
-      const result = await newsCollection.find().skip(skipPages*perPageData).limit(perPageData).toArray();
+      console.log(skipPages, perPageData)
+      const result = await newsCollection.find().skip(skipPages * perPageData).limit(perPageData).toArray();
       res.send(result);
     })
-  
+    app.get('/categoryNews', async (req, res) => {
+      const skipPages = parseInt(req.query.skipPages)
+      const category = req.query.category
+      const perPageData = parseInt(req.query.pageData)
+      const query = { category: category }
+      const result = await newsCollection.findOne(query).skip(skipPages * perPageData).limit(perPageData).toArray();
+      res.send(result);
+    })
+
+    app.patch('/news/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedNews = {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+          category: req.body.category,
+          image: req.body.image,
+          news: req.body.news
+        }
+      }
+      const result = await newsCollection.updateOne(filter, updatedNews, options);
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Twist buzz is running")
+  res.send("Twist buzz is running")
 })
 app.listen(port, () => {
-    console.log(`The app is running on port ${port}`)
+  console.log(`The app is running on port ${port}`)
 })
 
