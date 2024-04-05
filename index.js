ab722a524c183dfed7f5fc7e72fd7fe1e91891eb
mashuq0068 <mashuq0068@gmail.com>
2024-04-05 17:15:50 +0600

    added search api

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function run() {
     await client.connect();
     const database = client.db("twistBuzzDB")
     const newsCollection = database.collection("news")
+    const userCollection = database.collection("users")
     app.post('/news', async (req, res) => {
       const news = req.body
       const result = await newsCollection.insertOne(news)
@@ -39,19 +40,29 @@ async function run() {
 
     app.get('/allNews', async (req, res) => {
       const skipPages = parseInt(req.query.skipPages)
-      const perPageData = parseInt(req.query.pageData)
-      console.log(skipPages, perPageData)
+      const perPageData = parseInt(req.query.perPageData)
       const result = await newsCollection.find().skip(skipPages * perPageData).limit(perPageData).toArray();
       res.send(result);
     })
     app.get('/categoryNews', async (req, res) => {
       const skipPages = parseInt(req.query.skipPages)
       const category = req.query.category
-      const perPageData = parseInt(req.query.pageData)
+      const perPageData = parseInt(req.query.perPageData)
       const query = { category: category }
-      const result = await newsCollection.findOne(query).skip(skipPages * perPageData).limit(perPageData).toArray();
+      const result = await newsCollection.find(query).skip(skipPages * perPageData).limit(perPageData).toArray();
       res.send(result);
     })
+    app.get('/searchedNews', async (req, res) => {
+      try {
+        const searchedText = req.query.searchedText;
+        const query = { title: { $regex: searchedText, $options: 'i' } };
+        const result = await newsCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
 
     app.patch('/news/:id', async (req, res) => {
       const id = req.params.id
@@ -68,6 +79,33 @@ async function run() {
       }
       const result = await newsCollection.updateOne(filter, updatedNews, options);
       res.send(result)
+    })
+    app.delete('/news/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await newsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+    // for users
+    app.post('/user', async (req, res) => {
+      const user = req.body
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    }
+    )
+    app.get('/user/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const user = await newsCollection.findOne(user)
+      res.send(user)
+    })
+    app.get('/users', async (req, res) => {
+      const skipPages = parseInt(req.query.skipPages)
+      const perPageData = parseInt(req.query.perPageData)
+      const result = await userCollection.find().skip(skipPages * perPageData).limit(perPageData).toArray();
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });
